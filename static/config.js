fc8f41ae42662c2a8d07f8957695a8476187661a
gmantuanoconsulenti <g.mantuano@consulenti.gedidigital.it>
2025-08-19 19:40:20 +0200

    test

---
+++ static/config.js
@@ -0,0 +1,270 @@
+var conf = {
+  "Site": "cosicomodo.it/mercato",
+  "Sections": [
+    {
+      "Section": "hp",
+      "AdSlots": [
+        {
+          "slotId": "top_3",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": ["fluid", [1170, 320], [970, 250], [728, 90], [990, 90], [990, 120]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": ["fluid", [300, 100], [300, 50], [320, 50], [320, 100]]
+            }
+          ]
+        },
+        {
+          "slotId": "top_4",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": ["fluid", [1170, 320], [970, 250], [728, 90], [990, 90], [990, 120]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": ["fluid", [300, 100], [300, 50], [320, 50], [320, 100]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle1_C",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": ["fluid", [400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": ["fluid", [300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle2_C",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": ["fluid", [400, 300]]
+            },
+            {
+              "Device": "Mobile",
+              "sizes": ["fluid", [300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle3_C",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": ["fluid", [400, 300]]
+            },
+            {
+              "Device": "Mobile",
+              "sizes": ["fluid", [400, 300]],
+            }
+          ]
+        },
+        {
+          "slotId": "Middle4_C",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle5_C",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle6_C",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle7_C",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle8_C",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle9_C",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle1_M",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": ["fluid", [400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": ["fluid", [300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle2_M",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": ["fluid", [400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": ["fluid", [300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle3_M",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": ["fluid", [400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": ["fluid", [300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle4_M",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle5_M",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 250]]
+            }
+          ]
+        },
+        {
+          "slotId": "Middle6_M",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[400, 300]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 250]]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "Section": "Categ",
+      "AdSlots": [
+        {
+          "slotId": "top",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": [[970, 250], "fluid", [750, 200], [750, 90], [990, 90], [990, 120]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": [[300, 100], "fluid", [300, 50], [320, 50], [320, 100]]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "Section": "Ricerca",
+      "AdSlots": [
+        {
+          "slotId": "top",
+          "Ads": [
+            {
+              "Device": "Desktop",
+              "sizes": ["fluid", [970, 250], [728, 90], [990, 90], [990, 120]],
+            },
+            {
+              "Device": "Mobile",
+              "sizes": ["fluid", [300, 100], [300, 50], [320, 50], [320, 100]]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+
+      
